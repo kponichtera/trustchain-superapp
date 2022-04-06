a0f5c1358984b378708fb08e7778eadf7ca75ab8
Konrad Ponichtera <konpon96@gmail.com>
2022-04-06 18:39:36 +0200

    Commonized trade lists and making trade logic into the activity

--- atomic-swap/src/main/java/nl/tudelft/trustchain/atomicswap/ui/tradeoffers/TradeOffersFragment.kt
+++ atomic-swap/src/main/java/nl/tudelft/trustchain/atomicswap/ui/tradeoffers/TradeOffersFragment.kt
@@ -19,6 +19,7 @@ import java.math.BigDecimal
 
 class TradeOffersFragment : BaseFragment(R.layout.fragment_atomic_trade_offers) {
 
+
     private var _binding: FragmentAtomicTradeOffersBinding? = null
     private var _tradeOffersAdapter: ItemAdapter? = null
 
@@ -27,6 +28,7 @@ class TradeOffersFragment : BaseFragment(R.layout.fragment_atomic_trade_offers) 
     private val binding get() = _binding!!
 
     private val tradeOffersAdapter get() = _tradeOffersAdapter!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
